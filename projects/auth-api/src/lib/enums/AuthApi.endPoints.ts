24c3bcc98a7b31bc092062cc3f1487966e63d6b2
MostafaAbobaker <35193789+MostafaAbobaker@users.noreply.github.com>
2025-03-20 01:01:07 +0200

    Add environment

--- projects/auth-api/src/lib/enums/AuthApi.endPoints.ts
+++ projects/auth-api/src/lib/enums/AuthApi.endPoints.ts
@@ -1,13 +1,13 @@
 
 export class AuthApiEndPoints {
-  static SIGNUP           = "https://exam.elevateegy.com/api/v1/auth/signup";
-  static LOGIN            = "https://exam.elevateegy.com/api/v1/auth/signin";
-  static CHANGE_PASSWORD  = "https://exam.elevateegy.com/api/v1/auth/changePassword";
-  static DELETE_ACCOUNT   = "https://exam.elevateegy.com/api/v1/auth/deleteMe";
-  static EDIT_PROFILE     = "https://exam.elevateegy.com/api/v1/auth/editProfile";
-  static LOGOUT           = "https://exam.elevateegy.com/api/v1/auth/logout";
-  static USER_INFO        = "https://exam.elevateegy.com/api/v1/auth/profileData";
-  static FORGOT_PASSWORD  = "https://exam.elevateegy.com/api/v1/auth/forgotPassword";
-  static VERIFY_CODE      = "https://exam.elevateegy.com/api/v1/auth/verifyResetCode";
-  static RESET_PASSWORD   = "https://exam.elevateegy.com/api/v1/auth/resetPassword";
+  static SIGNUP           = "auth/signup";
+  static LOGIN            = "auth/signin";
+  static CHANGE_PASSWORD  = "auth/changePassword";
+  static DELETE_ACCOUNT   = "auth/deleteMe";
+  static EDIT_PROFILE     = "auth/editProfile";
+  static LOGOUT           = "auth/logout";
+  static USER_INFO        = "auth/profileData";
+  static FORGOT_PASSWORD  = "auth/forgotPassword";
+  static VERIFY_CODE      = "auth/verifyResetCode";
+  static RESET_PASSWORD   = "auth/resetPassword";
 }
